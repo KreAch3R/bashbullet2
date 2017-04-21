85c5e54c1bbb7cd07de4c6605286a75e17233776
Boteium <boteium@users.noreply.github.com>
2017-04-22 00:24:29 +0800

    update handler

--- config_dir/handler/mirror.sh
+++ config_dir/handler/mirror.sh
@@ -11,12 +11,12 @@ if [ $# == 4 ];then
 	ICO=$(mktemp -u --suffix=pushicon)
 	echo $4|base64 -d > "${ICO}"
 	convert ${ICO} -resize 60x60 ${ICO}.png
-	notify-send -i ${ICO}.png "$1 >> $2 \n$3" "$( cat - )"
+	notify-send -i ${ICO}.png "${1} >> ${2} \n${3}" "$( cat - )"
 	sleep 2;
 	rm "${ICO}"
 	rm "${ICO}.png"
 else
-	notify-send "$1 >> $2 \n$3" "$( cat - )"
+	notify-send "${1} >> ${2} \n${3}" "$( cat - )"
 fi
 
 if [ ! -z "$notify_command_mirror" ];then
